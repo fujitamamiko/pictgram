c32be124c4213759b30e86f9efd2ce2ca496926a
fujita mamiko <fjt115fjt@gmail.com>
2021-01-23 00:29:32 +0000

    first commit

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 15 }
+  
+  EMAIL_REGEX = /[a-zA-Z0-9._+]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*/
+  validates :email, presence: true, format: { with: EMAIL_REGEX }
+  
+  PW_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,32}\z/i
+  validates :password, presence: true, format: { with: PW_REGEX }
+
+  has_secure_password
+end
